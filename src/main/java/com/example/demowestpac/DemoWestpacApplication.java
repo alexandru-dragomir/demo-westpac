14907991b397795afe61a4a53dc04973f27bd782
alexandru-dragomir <117586362+alexandru-dragomir@users.noreply.github.com>
2023-01-27 12:51:41 +0200

    Update DemoWestpacApplication.java

--- src/main/java/com/example/demowestpac/DemoWestpacApplication.java
+++ src/main/java/com/example/demowestpac/DemoWestpacApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoWestpacApplication {
-
     public static void main(String[] args) {
+        String token = "SECRET_TOKEN_12315345345345345345";
         SpringApplication.run(DemoWestpacApplication.class, args);
     }
 
